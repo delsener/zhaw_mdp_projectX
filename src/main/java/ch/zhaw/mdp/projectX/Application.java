d4719c262013e4ce401cdfd96579bf8963539781
David Elsener <david.elsener@gmx.net>
2012-09-16 16:29:30 +0200

    initial version

---
+++ src/main/java/ch/zhaw/mdp/projectX/Application.java
@@ -0,0 +1,28 @@
+package ch.zhaw.mdp.projectX;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+
+
+/**
+ * Class containing the main method in which the application runs.
+ * 
+ * @author Roger Knecht, David Elsener
+ *
+ */
+public class Application {
+
+	private static final Logger LOGGER = Logger.getLogger(Application.class);
+	
+	/**
+	 * Main method.
+	 * 
+	 * @param args the program arguments.
+	 */
+	public static void main(String[] args) {
+		DOMConfigurator.configure("log4j.xml");
+		LOGGER.info("I'm such a cool project.");
+	}
+
+}
---
+++ src/test/java/ch/zhaw/mdp/projectX/ApplicationTest.java
@@ -0,0 +1,21 @@
+package ch.zhaw.mdp.projectX;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Tests the base functionality of this application.
+ * 
+ * @author Roger Knecht, David Elsener
+ *
+ */
+public class ApplicationTest {
+
+	@Test
+	public void testApplication() throws Exception {
+		// hopefully this will never fail
+		Assert.assertSame("everyone loves banana!", "i love banana", "i love banana");
+		Assert.assertNotSame("why everone should love apples?", "i hate apples", "i love apple pie");
+	}
+	
+}
